97ff0f29255b44903bc6b7ebc2644ecffb59f8db
Joey Innes <joey@inneslabs.uk>
2024-04-16 10:27:33 +0200

    Initial commit

---
+++ http/http.go
@@ -0,0 +1,191 @@
+package http
+
+import (
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/intob/dave/godave"
+	"github.com/intob/dave/godave/dave"
+	"golang.org/x/time/rate"
+)
+
+type App struct {
+	dave     *godave.Dave
+	work     int
+	ratelim  time.Duration
+	burst    int
+	lap      string
+	tlsCert  string
+	tlsKey   string
+	version  string
+	started  time.Time
+	clientMu sync.Mutex
+	clients  map[string]*client
+}
+
+type Cfg struct {
+	Dave      *godave.Dave
+	Work      int `yaml:"work"`
+	Version   string
+	Lap       string        `yaml:"lap"`
+	Ratelimit time.Duration `yaml:"ratelimit"`
+	Burst     int           `yaml:"burst"`
+	TLSCert   string        `yaml:"tlscert"`
+	TLSKey    string        `yaml:"tlskey"`
+}
+
+type client struct {
+	limiter  *rate.Limiter
+	lastSeen time.Time
+}
+
+func RunApp(cfg *Cfg) {
+	app := &App{
+		dave:    cfg.Dave,
+		work:    cfg.Work,
+		ratelim: cfg.Ratelimit,
+		burst:   cfg.Burst,
+		lap:     cfg.Lap,
+		tlsCert: cfg.TLSCert,
+		tlsKey:  cfg.TLSKey,
+		started: time.Now(),
+		version: cfg.Version,
+		clients: make(map[string]*client),
+	}
+	go app.cleanupClients()
+	app.serve()
+}
+
+func (app *App) serve() {
+	mux := http.NewServeMux()
+	mux.Handle("/", app.rateLimitMiddleware(
+		app.corsMiddleware(
+			http.HandlerFunc(app.handleRequest))))
+	server := &http.Server{Addr: app.lap, Handler: mux}
+	if app.tlsCert != "" {
+		fmt.Printf("app listening https on %s\n", app.lap)
+		err := server.ListenAndServeTLS(app.tlsCert, app.tlsKey)
+		if err != nil && err != http.ErrServerClosed {
+			panic(fmt.Sprintf("failed to listen https: %v\n", err))
+		}
+	}
+	fmt.Printf("listening http on %s\n", app.lap)
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		panic(fmt.Sprintf("failed to listen http: %v\n", err))
+	}
+}
+
+func (app *App) handleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+	if r.Method == "GET" {
+		head, err := hex.DecodeString(r.URL.Path[1:])
+		if err != nil {
+			http.Error(w, fmt.Sprintf("err decoding head: %v", err), 400)
+			return
+		}
+		if len(head) != 32 {
+			http.Error(w, "input must be of length 32 bytes", 400)
+			return
+		}
+		result := make([]byte, 0)
+		for dat := range getFile(app.dave, app.work, head) {
+			result = append(dat, result...)
+		}
+		w.Write(result)
+	}
+}
+
+func getFile(d *godave.Dave, work int, head []byte) <-chan []byte {
+	out := make(chan []byte)
+	go func() {
+		d.Send <- &dave.Msg{
+			Op:   dave.Op_GETDAT,
+			Work: head,
+		}
+		var i int
+		for m := range d.Recv {
+			if m.Op == dave.Op_DAT && bytes.Equal(m.Work, head) {
+				check := godave.CheckWork(m)
+				if check < work {
+					fmt.Printf("invalid work: %v, require: %v", check, work)
+					close(out)
+				}
+				out <- m.Val
+				head = m.Prev
+				i++
+				fmt.Printf("GOT DAT %d PREV::%x\n", i, head)
+				if head == nil {
+					close(out)
+					return
+				}
+			send:
+				for {
+					select {
+					case d.Send <- &dave.Msg{
+						Op:   dave.Op_GETDAT,
+						Work: head,
+					}:
+						break send
+					case <-d.Recv:
+					}
+				}
+
+			}
+		}
+	}()
+	return out
+}
+
+func (app *App) corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *App) rateLimitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limiter := app.getRateLimiter(r)
+		if !limiter.Allow() {
+			w.WriteHeader(http.StatusTooManyRequests)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *App) getRateLimiter(r *http.Request) *rate.Limiter {
+	app.clientMu.Lock()
+	defer app.clientMu.Unlock()
+	key := r.Method + r.RemoteAddr
+	v, exists := app.clients[key]
+	if !exists {
+		limiter := rate.NewLimiter(rate.Every(app.ratelim), app.burst)
+		app.clients[key] = &client{limiter, time.Now()}
+		return limiter
+	}
+	v.lastSeen = time.Now()
+	return v.limiter
+}
+
+func (a *App) cleanupClients() {
+	for {
+		<-time.After(10 * time.Second)
+		a.clientMu.Lock()
+		for key, client := range a.clients {
+			if time.Since(client.lastSeen) > 10*time.Second {
+				delete(a.clients, key)
+			}
+		}
+		a.clientMu.Unlock()
+	}
+}
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"net/netip"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/intob/dave/godave"
+	"github.com/intob/daveg/http"
+)
+
+func main() {
+	network := flag.String("network", "udp", "<udp|udp6|udp4>")
+	lap := flag.String("l", ":80", "<LAP> listen address:port")
+	bap := flag.String("b", "", "<BAP> bootstrap address:port")
+	bfile := flag.String("bf", "", "<BFILE> bootstrap file of address:port\\n")
+	work := flag.Int("w", 3, "<WORK_MIN> minimum work to store DAT")
+	flag.Parse()
+	d := runDave(*network, *lap, *bfile, *bap, *work)
+	go http.RunApp(&http.Cfg{
+		Dave:      d,
+		Work:      *work,
+		Version:   "1234",
+		Lap:       *lap,
+		Ratelimit: 300 * time.Millisecond,
+		Burst:     10,
+	})
+	<-make(chan struct{})
+}
+
+func runDave(network, lap, bfile, bap string, work int) *godave.Dave {
+	bootstrap := make([]netip.AddrPort, 0)
+	if bap != "" {
+		if strings.HasPrefix(bap, ":") {
+			bap = "[::1]" + bap
+		}
+		addr, err := netip.ParseAddrPort(bap)
+		if err != nil {
+			exit(1, "failed to parse -b=%q: %v", bap, err)
+		}
+		bootstrap = append(bootstrap, addr)
+	}
+	if bfile != "" {
+		bootstrap = append(bootstrap, readBaps(bfile)...)
+	}
+
+	laddr, err := net.ResolveUDPAddr(network, lap)
+	if err != nil {
+		exit(1, "failed to resolve UDP address: %v", err)
+	}
+	d, err := godave.NewDave(work, laddr, bootstrap)
+	if err != nil {
+		exit(1, "failed to make dave: %v", err)
+	}
+
+	var n int
+	fmt.Printf("%v\nbootstrap\n", bootstrap)
+	for range d.Recv {
+		n++
+		fmt.Printf(".\033[0K")
+		if n >= 8 {
+			fmt.Print("\n\033[0K")
+			break
+		}
+	}
+	return d
+}
+
+func readBaps(fname string) []netip.AddrPort {
+	addrs := make([]netip.AddrPort, 0)
+	f, err := os.Open(fname)
+	if err != nil {
+		exit(1, "readBaps failed: %v", err)
+	}
+	defer f.Close()
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		l := s.Text()
+		if l != "" && !strings.HasPrefix(l, "#") {
+			l = strings.ReplaceAll(l, "\t", " ")
+			fields := strings.Split(l, " ")
+			if len(fields) == 0 {
+				continue
+			}
+			addr, err := netip.ParseAddrPort(fields[0])
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			addrs = append(addrs, addr)
+		}
+	}
+	return addrs
+}
+
+func exit(code int, msg string, args ...any) {
+	fmt.Printf(msg, args...)
+	os.Exit(code)
+}
